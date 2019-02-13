c13f62ddcd9d2b2892ead56350d0323256ed72ab
Kenneth Kan <kenhkan@gmail.com>
2019-02-12 20:29:07 -0500

    Create two records

--- index.js
+++ index.js
@@ -38,69 +38,74 @@ function endOfProgram() {
   }
 }
 
+function goThroughRecord(record, periodCount) {
+  const startDate = moment(record.get("Start date")).startOf("day");
+  const endDate = record.get("End date") && moment(record.get("End date")).startOf("day");
+  const period = record.get("Period (days)");
+  const startToTargetInDays = Math.ceil(Math.abs(startDate.diff(today, "days")) / period) * period * periodCount;
+  const dueDate = startDate.clone().add(startToTargetInDays, "days");
+  const workStartInSeconds = secondsSinceMidNight(record.get("Work start"));
+  const workStartDate = workStartInSeconds && dueDate.clone().add(workStartInSeconds, "seconds") || null;
+  const workEndInSeconds  = secondsSinceMidNight(record.get("Work end"));
+  const workEndDate = workEndInSeconds && dueDate.clone().add(workEndInSeconds, "seconds") || dueDate;
+  const summary = record.get("Summary");
+  const filterFormula = "AND({Summary} = '" + summary + "', DATETIME_FORMAT({Work end}) = '" + workEndDate.clone().utc().format("YYYY-MM-DDTHH:mm:ss+00:00") + "')";
+
+  console.log("Looking at ", summary, "...");
+
+  if (startDate && startDate.isAfter(today)) {
+    console.log("Series not started.");
+    return;
+  }
+
+  if (endDate && endDate.isBefore(workEndDate)) {
+    console.log("Series ended.");
+    return;
+  }
+
+  const itemId = itemCount++;
+
+  console.log(itemId, "Looking for existing record...");
+
+  base(toTable).select({
+    filterByFormula: filterFormula
+  }).eachPage(function page(existing, _) {
+    if (existing.length > 0) {
+      console.log(itemId, "Existing record found.");
+      itemCount--;
+      endOfProgram();
+      return;
+    }
+
+    console.log(itemId, "Creating new record...");
+
+    base(toTable).create({
+      "Summary": summary,
+      "Work start": workStartDate,
+      "Work end": workEndDate,
+      "Due": dueDate.format("YYYY-MM-DD"),
+      "Notes": record.get("Notes"),
+      "Project": record.get("Project"),
+      "Grouping": "Recurring"
+    }, function(err, record) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(itemId, "New record created.");
+      }
+
+      itemCount--;
+      endOfProgram();
+    });
+  });
+}
+
 base(fromTable).select({
   view: fromView
 }).eachPage(function page(records, fetchNextPage) {
   records.forEach(function(record) {
-    const startDate = moment(record.get("Start date")).startOf("day");
-    const endDate = record.get("End date") && moment(record.get("End date")).startOf("day");
-    const period = record.get("Period (days)");
-    const startToTargetInDays = Math.ceil(Math.abs(startDate.diff(today, "days")) / period) * period;
-    const dueDate = startDate.clone().add(startToTargetInDays, "days");
-    const workStartInSeconds = secondsSinceMidNight(record.get("Work start"));
-    const workStartDate = workStartInSeconds && dueDate.clone().add(workStartInSeconds, "seconds") || null;
-    const workEndInSeconds  = secondsSinceMidNight(record.get("Work end"));
-    const workEndDate = workEndInSeconds && dueDate.clone().add(workEndInSeconds, "seconds") || dueDate;
-    const summary = record.get("Summary");
-    const filterFormula = "AND({Summary} = '" + summary + "', DATETIME_FORMAT({Work end}) = '" + workEndDate.clone().utc().format("YYYY-MM-DDTHH:mm:ss+00:00") + "')";
-
-    console.log("Looking at ", summary, "...");
-
-    if (startDate && startDate.isAfter(today)) {
-      console.log("Series not started.");
-      return;
-    }
-
-    if (endDate && endDate.isBefore(workEndDate)) {
-      console.log("Series ended.");
-      return;
-    }
-
-    const itemId = itemCount++;
-
-    console.log(itemId, "Looking for existing record...");
-
-    base(toTable).select({
-      filterByFormula: filterFormula
-    }).eachPage(function page(existing, _) {
-      if (existing.length > 0) {
-        console.log(itemId, "Existing record found.");
-        itemCount--;
-        endOfProgram();
-        return;
-      }
-
-      console.log(itemId, "Creating new record...");
-
-      base(toTable).create({
-        "Summary": summary,
-        "Work start": workStartDate,
-        "Work end": workEndDate,
-        "Due": dueDate.format("YYYY-MM-DD"),
-        "Notes": record.get("Notes"),
-        "Project": record.get("Project"),
-        "Grouping": "Recurring"
-      }, function(err, record) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(itemId, "New record created.");
-        }
-
-        itemCount--;
-        endOfProgram();
-      });
-    });
+    goThroughRecord(record, 1);
+    goThroughRecord(record, 2);
   });
 
   fetchNextPage();
